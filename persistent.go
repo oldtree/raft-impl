f9b314ad11ac22f48f8ed3a27ca873af011c3b90
suoyanan <suoyanan@sensetime.com>
2018-06-19 16:28:15 +0800

    添加持久化的一些接口

--- persistent.go
+++ persistent.go
@@ -14,5 +14,53 @@ type Persistent interface {
 type FilePersistent struct {
 }
 
+func (fi *FilePersistent) CreateDir(path string) error {
+	return nil
+}
+
+func (fi *FilePersistent) RemoveDir(path string) error {
+	return nil
+}
+
+func (fi *FilePersistent) CreateFile(filename string) error {
+	return nil
+}
+
+func (fi *FilePersistent) RemoveFile(filename string) error {
+	return nil
+}
+
+func (fi *FilePersistent) Wirte([]byte) (int, error) {
+	return 0, nil
+}
+
+func (fi *FilePersistent) Read([]byte) (int, error) {
+	return 0, nil
+}
+
 type BoltDbPersisten struct {
+}
+
+func (bol *BoltDbPersisten) CreateDir(path string) error {
+	return nil
+}
+
+func (bol *BoltDbPersisten) RemoveDir(path string) error {
+	return nil
+}
+
+func (bol *BoltDbPersisten) CreateFile(filename string) error {
+	return nil
+}
+
+func (bol *BoltDbPersisten) RemoveFile(filename string) error {
+	return nil
+}
+
+func (bol *BoltDbPersisten) Wirte([]byte) (int, error) {
+	return 0, nil
+}
+
+func (bol *BoltDbPersisten) Read([]byte) (int, error) {
+	return 0, nil
 }
